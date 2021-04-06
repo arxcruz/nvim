dbfa751bce5ca0c14135d98d5c45b7fdf6ff0203
Arx Cruz <arxcruz@redhat.com>
2021-04-06 14:01:07 +0200

    Updating quit current windows
    Using vim.api instead of vim.cmd from lua files

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -2,11 +2,14 @@ vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true}
 vim.g.mapleader = ','
 
 -- Quit current window and all windows
-vim.cmd([[
-  noremap <Leader>e :quit<CR>
-  noremap <Leader>E :qa!<CR>
-]])
-
+vim.api.nvim_set_keymap('n', '<Leader>e', ':quit<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>E', ':qa!<CR>', {noremap = true, silent = true})
 
 -- nvim-tree
 vim.api.nvim_set_keymap('n', '<Space>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+
+-- Window movement
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
