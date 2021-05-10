b8a4d155029cd63e2776f983219e2303420ef720
Arx Cruz <arxcruz@gmail.com>
2021-05-10 13:17:01 +0200

    Update fzf commands

--- lua/fzf/init.lua
+++ lua/fzf/init.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_keymap('n', '<C-f', '<esc><esc>:Files!<CR>', {silent = true})
-vim.api.nvim_set_keymap('i', '<C-f', '<esc><esc>:BLines!<CR>', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-g', '<esc><esc>:GCommits!<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>f', '<esc><esc>:Files!<CR>', {silent = true})
+vim.api.nvim_set_keymap('i', '<Leader>f', '<esc><esc>:BLines!<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-g', '<esc><esc>:GCommits!<CR', {silent = true})
