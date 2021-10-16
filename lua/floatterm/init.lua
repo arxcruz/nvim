c4a4626c23a94287b12e84588ef79a10152f2943
Arx Cruz <arxcruz@redhat.com>
2021-10-16 23:09:07 +0200

    Update floatterm settings

--- lua/floatterm/init.lua
+++ lua/floatterm/init.lua
@@ -2,4 +2,4 @@ vim.g.floaterm_keymap_toggle = '<F1>'
 vim.g.floaterm_keymap_next   = '<F2>'
 vim.g.floaterm_keymap_prev   = '<F3>'
 vim.g.floaterm_keymap_new    = '<F4>'
-vim.g.floaterm_title=''
+vim.g.floaterm_height = 0.3
