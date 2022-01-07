f7eda6c27f021bdd4bcfce17ff80e101df5bbf9e
Arx Cruz <arxcruz@redhat.com>
2022-01-07 11:51:05 +0100

    Set terminal to open below

--- lua/floatterm/init.lua
+++ lua/floatterm/init.lua
@@ -3,3 +3,5 @@ vim.g.floaterm_keymap_next   = '<F2>'
 vim.g.floaterm_keymap_prev   = '<F3>'
 vim.g.floaterm_keymap_new    = '<F4>'
 vim.g.floaterm_height = 0.3
+vim.g.floaterm_wintype = 'split'
+vim.g.floaterm_position = 'belowright'
