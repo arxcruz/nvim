92dbd2ba312fc08d6d99f5fb2e4b0513029a50e6
Arx Cruz <arxcruz@gmail.com>
2022-04-11 22:23:13 +0200

    Update mappings
    Moved to Tab and Shift-Tab because it makes more sense

--- lua/nvimcmp/init.lua
+++ lua/nvimcmp/init.lua
@@ -19,8 +19,8 @@ cmp.setup({
         end,
     },
     mapping = {
-        ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-        ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
         ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ['<C-e>'] = cmp.mapping({
