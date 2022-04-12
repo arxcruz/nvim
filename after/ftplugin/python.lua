9c520d48d4096e447b944b590efae123162c7bc2
Arx Cruz <arxcruz@gmail.com>
2022-04-12 19:10:29 +0200

    Remove pylint
    Parsing the command argument is making it fail

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -11,9 +11,9 @@ local sources = {
     -- null_ls.builtins.formatting.black,
     -- null_ls.builtins.formatting.isort,
     null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.pylint.with({
-        command = "pylint-3",
-    }),
+    -- null_ls.builtins.diagnostics.pylint.with({
+    --     command = "pylint-3",
+    -- }),
 }
 
 null_ls.setup({sources = sources, debug = true, })
