7ec2da9ae83aabfcd656a0106953e2c891fa97dd
Arx Cruz <arxcruz@gmail.com>
2022-10-24 15:23:56 +0200

    Change default_capabilities to update_capabilities
    The first is deprecated now

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -176,7 +176,7 @@ lsp_installer.settings({
 lsp_installer.on_server_ready(function(server)
     local opts = {
         on_attach = on_attach,
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp
                                                                        .protocol
                                                                        .make_client_capabilities()),
         flags = {debounce_text_changes = 150}
