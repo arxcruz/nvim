c4c5edd00a4a8cb3449557b7ecee501dfd55326d
Arx Cruz <arxcruz@redhat.com>
2022-10-24 17:18:14 +0200

    Updating treesitter settings

--- lua/treesitter/init.lua
+++ lua/treesitter/init.lua
@@ -1,6 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {'python', 'go'},
   sync_install = false,
+  autopairs = {
+      enable = true
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
